cd9fb4975de5d69408128c2c48889dde1608a8e7
shreethaar <shreethaara380@gmail.com>
2025-11-15 03:51:10 +0800

    sess1

---
+++ README.md
@@ -0,0 +1,4 @@
+# rust for win off
+- rust offensive development for windows from course pointerSec 
+- include rust basic from [rust by example](https://doc.rust-lang.org/stable/rust-by-example/index.html)
+
---
+++ rust-by-example/comments.rs
@@ -0,0 +1,18 @@
+// Line comments 
+/* Block comments 
+ *
+ */
+
+fn main() {
+    // comment #1
+    println!("Now");
+    println!("everything");
+    println!("executes!");
+
+    // comment #2
+
+
+    let x = 5 + /* 90 + */ 5; // manipulate expressions easily with block comments 
+    println!("Is `x` 10 or 100? x = {}",x);
+}
+
